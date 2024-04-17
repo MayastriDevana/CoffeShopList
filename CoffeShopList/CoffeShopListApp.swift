593fe55a98902e06b328df24b9e2be8b2834c29d
Maya <Mayastridevana0205@gmail.com>
2024-04-17 13:07:18 +0700

    Initial Commit

---
+++ CoffeShopList/CoffeShopListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopListApp.swift
+//  CoffeShopList
+//
+//  Created by User on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
